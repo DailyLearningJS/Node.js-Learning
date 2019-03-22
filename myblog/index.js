2ed7ea7a8fd1fa36badb0ccbb4ae026c33fb6640
weijiyang <crawling_snail@163.com>
2019-03-22 19:44:28 +0800

    node superviso express router

---
+++ myblog/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/user')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', indexRouter)
+app.use('/users', userRouter)
+
+app.listen(3000)
